b56fbe6d128030e64e7fa7da7932e278e01b6ae1
br0nt0 <babilon5000@gmail.com>
2024-02-13 16:24:31 +0200

    #4 created neuron computation

--- src/neuron.c
+++ src/neuron.c
@@ -4,7 +4,31 @@
  ******************************************************************************/
 #include "neuron.h"
 
-void a( void )
+/******************************************************************************/
+
+/******************************************************************************/
+
+/******************************************************************************/
+float32_t getNeuronOutput(	const float32_t* weights,
+							float32_t bias,
+							const activationHandler handler,
+							const float32_t* input,
+							uint8_t inputSize )
 {
-    
+	float32_t output = 0.0f;
+
+	if ( input && weights )
+	{
+		output = bias;
+		for ( uint8_t i = 0; i < inputSize; i++ )
+		{
+			output += weights[ i ] * input[ i ];
+		}
+		if ( NULL != handler )
+		{
+			output = handler( output );
+		}
+
+	}
+	return ( output );
 }
--- src/neuron.h
+++ src/neuron.h
@@ -4,6 +4,16 @@
  ******************************************************************************/
 #ifndef NEURON_H_
 #define NEURON_H_
+/******************************************************************************/
+#include "activationFunctions.h"
+#include "localTypes.h"
+
+/******************************************************************************/
+float32_t getNeuronOutput(	const float32_t* weights,
+							float32_t bias,
+							const activationHandler handler,
+							const float32_t* input,
+							uint8_t inputSize );
 
 
 
